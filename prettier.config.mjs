ed83af1a6c3218a693d1edc4e5fb36bd4496e4f7
sheikhmohdnazmulhasan <nazmulofficial@outlook.com>
2026-06-02 18:29:50 +0600

    Update project configuration and dependencies. Added .npmrc and .nvmrc files for package management and Node versioning. Enhanced .gitignore and .prettierignore to exclude additional files. Updated package.json with new scripts and dependencies, including eslint-config-prettier and prettier. Refactored global CSS for improved styling consistency and added a Prettier configuration file.

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const prettierConfig = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
+
+export default prettierConfig;
